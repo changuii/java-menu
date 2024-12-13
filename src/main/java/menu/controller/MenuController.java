c930c6a505e3e5d67fd2d02a72d3700c9daa82f2
changuii <rhljh7410@gmail.com>
2024-12-13 14:55:18 +0900

    feat : view와 domain을 제어하기위한 MenuController를 작성하라

---
+++ src/main/java/menu/controller/MenuController.java
@@ -0,0 +1,15 @@
+package menu.controller;
+
+import menu.view.InputView;
+
+public class MenuController {
+    private final InputView inputView;
+
+    public MenuController(final InputView inputView) {
+        this.inputView = inputView;
+    }
+
+    public void run() {
+        inputView.readCoachNames();
+    }
+}
